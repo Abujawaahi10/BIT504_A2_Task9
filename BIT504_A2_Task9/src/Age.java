5cc9dbff88d0e4481d9d658541ba302800eaf6b5
Abujawaahir10 <abuudi142@gmail.com>
2025-10-02 21:09:56 +1300

    Initial commit with Age.java

---
+++ BIT504_A2_Task9/src/Age.java
@@ -0,0 +1,11 @@
+// BIT504 - Assessment 2
+// Name: Abdullah Hagi
+// Student ID: 5135592
+// Assessment: BIT504_A2
+
+public class Age {
+    public static void main(String[] args) {
+        int age = 25;
+        System.out.println("Age: " + age);
+    }
+}
